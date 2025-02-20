9b2250f5c988dceaae1399517907696021d021b6
Jacob Hall <bubba.hall2003@gmail.com>
2025-02-20 12:23:51 -0700

    changes to profile.css & profile.jsx

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -11,52 +11,29 @@ export function Profile() {
                         <button type="submit">Logout</button>
                     </form>
             </div>
-            <h3>Top 6 Movies</h3>
-            <div className="movie-list">
-                <div className="movie-card">
-                    <span style={{display: "inline-block", marginRight: "20px", textAlign: "center"}}>
-                        <h4 style={{ display: "inline"}}>1. Se7en </h4>
-                        <br />
-                        <img src="https://tinyurl.com/4tvma26z" alt="Se7en" width="100" />
-                    </span>
+            <h3>Famous Movies and Quotes</h3>
+            <div className="movie-quote-list">
+                <div className="movie-quote-card">
+                    <h4>1. Se7en</h4>
+                    <img src="https://tinyurl.com/4tvma26z" alt="Se7en" width="100" />
+                    <p>"What's in the box?"</p>
                 </div>
-                <div className="movie-card">
-                    <span style={{display: "inline-block", marginRight: "20px", textAlign: "center"}}>
-                        <h4 style={{ display: "inline"}}>2. Mystic River </h4>
-                        <br />
-                        <img src="https://tinyurl.com/m2jtss8d" alt="Mystic River" width="100" />
-                    </span>
+                <div className="movie-quote-card">
+                    <h4>2. Mystic River</h4>
+                    <img src="https://tinyurl.com/m2jtss8d" alt="Mystic River" width="100" />
+                    <p>"Is that my daughter in there?"</p>
                 </div>
-                <div className="movie-card">
-                    <span style={{display: "inline-block", marginRight: "20px", textAlign: "center"}}>
-                        <h4 style={{ display: "inline"}}>3. Gladiator</h4>
-                        <br />
-                        <img src="https://tinyurl.com/4zb8pz5d" alt="Gladiator" width="100" />
-                    </span>
-                    
+                <div className="movie-quote-card">
+                    <h4>3. Gladiator</h4>
+                    <img src="https://tinyurl.com/4zb8pz5d" alt="Gladiator" width="100" />
+                    <p>"Are you not entertained?"</p>
                 </div>
-                <div className="movie-card">
-                    <span style={{display: "inline-block", marginRight: "20px", textAlign: "center"}}>
-                        <h4 style={{ display: "inline"}}>4. American Gangster </h4>
-                        <br />
-                        <img src="https://tinyurl.com/36uaya6h" alt="American Gangster" width="100" />
-                    </span>
+                <div className="movie-quote-card">
+                    <h4>4. There Will Be Blood</h4>
+                    <img src="https://tinyurl.com/bdhsph9e" alt="There Will Be Blood" width="100" />
+                    <p>"I drink your milkshake!"</p>
                 </div>
-                <div className="movie-card">
-                    <span style={{display: "inline-block", marginRight: "20px", textAlign: "center"}}>
-                        <h4 style={{ display: "inline"}}>5. Hell or High Water </h4>
-                        <br />
-                        <img src="https://tinyurl.com/2z8wksv3" alt="Interstellar" width="100" />
-                    </span>
-                </div>
-                <div className="movie-card">
-                    <span style={{display: "inline-block", marginRight: "20px", textAlign: "center"}}>
-                        <h4 style={{ display: "inline"}}>6. There Will Be Blood </h4>
-                        <br />
-                        <img src="https://tinyurl.com/bdhsph9e" alt="There Will Be Blood" width="100" />
-                    </span>
             </div>
-        </div>
         </main>
   );
 }

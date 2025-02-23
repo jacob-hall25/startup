cac1454c0e7612ade6625cfb01b290490c81b2ae
Jacob Hall <bubba.hall2003@gmail.com>
2025-02-22 17:58:33 -0700

    react components added to profile page

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -2,11 +2,41 @@ import React from 'react';
 import './profile.css';
 
 export function Profile() {
+    const [username, setUsername] = React.useState("bubba_hall25");
+    const [movie1Title, setMovie1Title] = React.useState("Se7en");
+    const [movie1Image, setMovie1Image] = React.useState("https://tinyurl.com/4tvma26z");
+    const [movie1Quote, setMovie1Quote] = React.useState("What's in the box?");
+    const [movie2Title, setMovie2Title] = React.useState("Mystic River");
+    const [movie2Image, setMovie2Image] = React.useState("https://tinyurl.com/m2jtss8d");
+    const [movie2Quote, setMovie2Quote] = React.useState("Is that my daughter in there?");
+    const [movie3Title, setMovie3Title] = React.useState("Gladiator");
+    const [movie3Image, setMovie3Image] = React.useState("https://tinyurl.com/4zb8pz5d");
+    const [movie3Quote, setMovie3Quote] = React.useState("Are you not entertained?");
+    const [movie4Title, setMovie4Title] = React.useState("There Will Be Blood");
+    const [movie4Image, setMovie4Image] = React.useState("https://tinyurl.com/bdhsph9e");
+    const [movie4Quote, setMovie4Quote] = React.useState("I drink your milkshake!");
+
+    React.useEffect(() => {
+        setUsername("bubba_hall25");
+        setMovie1Title("Se7en");
+        setMovie1Image("https://tinyurl.com/4tvma26z");
+        setMovie1Quote("What's in the box?");
+        setMovie2Title("Mystic River");
+        setMovie2Image("https://tinyurl.com/m2jtss8d");
+        setMovie2Quote("Is that my daughter in there?");
+        setMovie3Title("Gladiator");
+        setMovie3Image("https://tinyurl.com/4zb8pz5d");
+        setMovie3Quote("Are you not entertained?");
+        setMovie4Title("There Will Be Blood");
+        setMovie4Image("https://tinyurl.com/bdhsph9e");
+        setMovie4Quote("I drink your milkshake!");
+    }, []);
+
   return (
     <main className="container-fluid text-center profile-container">
         <hr />
             <div className="profile-header">
-                <h1 className="username">bubba_hall25</h1>
+                <h1 className="username">{username}</h1>
                     <form method="get" action="index.html" style={{ display: "inline"}}>
                         <button type="submit">Logout</button>
                     </form>
@@ -14,24 +44,24 @@ export function Profile() {
             <h3>Famous Movies and Quotes</h3>
             <div className="movie-quote-list">
                 <div className="movie-quote-card">
-                    <h4>1. Se7en</h4>
-                    <img src="https://tinyurl.com/4tvma26z" alt="Se7en" width="100" />
-                    <p>"What's in the box?"</p>
+                    <h4>{movie1Title}</h4>
+                    <img src={movie1Image} alt="random-movie" width="100" />
+                    <p>{movie1Quote}</p>
                 </div>
                 <div className="movie-quote-card">
-                    <h4>2. Mystic River</h4>
-                    <img src="https://tinyurl.com/m2jtss8d" alt="Mystic River" width="100" />
-                    <p>"Is that my daughter in there?"</p>
+                    <h4>{movie2Title}</h4>
+                    <img src={movie2Image} alt="random-movie" width="100" />
+                    <p>{movie2Quote}</p>
                 </div>
                 <div className="movie-quote-card">
-                    <h4>3. Gladiator</h4>
-                    <img src="https://tinyurl.com/4zb8pz5d" alt="Gladiator" width="100" />
-                    <p>"Are you not entertained?"</p>
+                    <h4>{movie3Title}</h4>
+                    <img src={movie3Image} alt="random-movie" width="100" />
+                    <p>{movie3Quote}</p>
                 </div>
                 <div className="movie-quote-card">
-                    <h4>4. There Will Be Blood</h4>
-                    <img src="https://tinyurl.com/bdhsph9e" alt="There Will Be Blood" width="100" />
-                    <p>"I drink your milkshake!"</p>
+                    <h4>{movie4Title}</h4>
+                    <img src={movie4Image} alt="random-movie" width="100" />
+                    <p>{movie4Quote}</p>
                 </div>
             </div>
         </main>

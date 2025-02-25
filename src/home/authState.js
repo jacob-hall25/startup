28eb7de9e57e2a372ecae581ba0e065279604b1c
Jacob Hall <bubba.hall2003@gmail.com>
2025-02-25 11:31:17 -0700

    new files created for login components / authState.js changes

---
+++ src/home/authState.js
@@ -0,0 +1,9 @@
+export class AuthState {
+    static Unknown = new AuthState('Unknown');
+    static Authenticated = new AuthState('Authenticated');
+    static Unauthenticated = new AuthState('Unauthenticated');
+
+    constructor(name) {
+        this.name = name;
+    }
+}

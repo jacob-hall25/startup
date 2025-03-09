e47577f995bbfa252996d3ec7685ab1eb198e56e
Jacob Hall <bubba.hall2003@gmail.com>
2025-03-09 17:21:07 -0600

    index.js changes. middleware and routes are added.

---
+++ service/index.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// app.get('*', (_req, res) => {
+//   res.send({ msg: 'Simon service' });
+// });
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+app.use(express.json());
+
+let users = [];
+let ratings = [];
+
+let apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+
+apiRouter.post('/auth/create', async (requestAnimationFrame, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'User already exists' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email});
+  }
+});
+
+apiRouter.post('auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.post('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
+apiRouter.post('/ratings', verifyAuth, (_req, res) => {
+  res.send(ratings);
+});
+
+apiRouter.post('/rating', verifyAuth, async (req, res) => {
+  ratings = updateRatings(req.body);
+  res.send(ratings);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+function addRating(newRating) {
+  ratings.push(newRating);
+  return ratings;
+}
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});

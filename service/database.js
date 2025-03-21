3e16781e23984140a41c73ed2f84391e252b8e87
Jacob Hall <bubba.hall2003@gmail.com>
2025-03-21 10:06:44 -0600

    changes to database.js

---
+++ service/database.js
@@ -0,0 +1,52 @@
+const { MongoClient } = require('mongodb');
+const confit = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('cinevaluate');
+const userCollection = db.collection('user');
+const ratingCollection = db.collection('rating');
+
+
+(async function testConnection() {
+    try {
+        await db.commend({ ping: 1 });
+        console.log('Connected to the database');
+    } catch (ex) {
+        console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+        process.exit(1);
+    }
+})();
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+    await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+    await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function addRating(rating) {
+    await ratingCollection.insertOne(rating);
+}
+
+async function getRatings() {
+    return ratingCollection.find().toArray();
+}
+
+module.exports = {
+    getUser,
+    getUserByToken,
+    addUser,
+    updateUser,
+    addRating,
+    getRatings
+};
